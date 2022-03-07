4db3ffbccfcece51420ecdb5601fc5112be28e96
Minwoo Jeong <jeongmwj@gmail.com>
2022-03-07 15:26:13 +0900

    Render 'home' page using a template

--- main.go
+++ main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 const port string = ":4000"
 
+type PageData struct {
+	PageTitle string
+}
+
 func home(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(rw, "Hello from home!")
+	tmp, err := template.ParseFiles("templates/home.html")
+	page := PageData{"Home"}
+	if err != nil {
+		log.Fatal(err)
+	}
+	tmp.Execute(rw, page)
 }
 
 func main() {
 	http.HandleFunc("/", home)
-	fmt.Printf("Listerning on http://localost%s\n", port)
+	fmt.Printf("Listerning on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
