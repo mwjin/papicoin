f3b38184f9e958877485f2e57661b52bacfea7de
Minwoo Jeong <mwjeong.sci@gmail.com>
2022-03-08 15:45:22 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "github.com/mwjjeong/papicoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			"/",
+			"GET",
+			"See Documentation",
+		},
+	}
+	b, err := json.Marshal(data)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Fprintf(rw, "%s", b)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	addr := fmt.Sprintf("http://localhost%s", port)
+	fmt.Printf("Listening on %s\n", addr)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
