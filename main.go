16dd24fe5eea4d4cf2c3236531ec608efafc155b
Minwoo Jeong <mwjeong.sci@gmail.com>
2022-03-08 14:51:23 +0900

    Implement add page

--- main.go
+++ main.go
@@ -26,10 +26,26 @@ func home(rw http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(rw, "home", data)
 }
 
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil)
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockchain().AddBlock(data)
+		fmt.Printf("Add the data '%s' to the chain.\n", data)
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+	default:
+		fmt.Println("Hello")
+	}
+}
+
 func main() {
 	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
 	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
 	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
 	fmt.Printf("Listerning on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
