3a63cbb8920ca830b6f90ff3d326ddc11b3fa366
Minwoo Jeong <mwjeong.sci@gmail.com>
2022-03-08 15:05:09 +0900

    Create 'explorer' package

---
+++ explorer/explorer.go
@@ -0,0 +1,51 @@
+package explorer
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/mwjjeong/papicoin/blockchain"
+)
+
+const (
+	port        string = ":4000"
+	templateDir string = "explorer/templates/"
+)
+
+var templates *template.Template
+
+type PageData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	data := PageData{"Home", blockchain.GetBlockchain().GetAllBlocks()}
+	templates.ExecuteTemplate(rw, "home", data)
+}
+
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil)
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockchain().AddBlock(data)
+		fmt.Printf("Add the data '%s' to the chain.\n", data)
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+	default:
+		fmt.Println("Hello")
+	}
+}
+
+func Start() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
+	fmt.Printf("Listerning on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
--- main.go
+++ main.go
@@ -1,51 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"html/template"
-	"log"
-	"net/http"
-
-	"github.com/mwjjeong/papicoin/blockchain"
-)
-
-const (
-	port        string = ":4000"
-	templateDir string = "templates/"
-)
-
-var templates *template.Template
-
-type PageData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
-}
-
-func home(rw http.ResponseWriter, r *http.Request) {
-	data := PageData{"Home", blockchain.GetBlockchain().GetAllBlocks()}
-	templates.ExecuteTemplate(rw, "home", data)
-}
-
-func add(rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		templates.ExecuteTemplate(rw, "add", nil)
-	case "POST":
-		r.ParseForm()
-		data := r.Form.Get("blockData")
-		blockchain.GetBlockchain().AddBlock(data)
-		fmt.Printf("Add the data '%s' to the chain.\n", data)
-		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
-	default:
-		fmt.Println("Hello")
-	}
-}
+import "github.com/mwjjeong/papicoin/explorer"
 
 func main() {
-	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
-	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-	http.HandleFunc("/", home)
-	http.HandleFunc("/add", add)
-	fmt.Printf("Listerning on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	explorer.Start()
 }
