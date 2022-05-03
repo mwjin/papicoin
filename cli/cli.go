65fc65416441c088d0ad0a9b2430d412d264b530
Minwoo Jeong <jeongmwj@gmail.com>
2022-05-03 21:01:37 +0900

    Add `both` mode

--- cli/cli.go
+++ cli/cli.go
@@ -10,15 +10,16 @@ import (
 )
 
 func Start() {
-	mode := flag.String("mode", "rest", "Choose between 'html' and 'rest'")
-	port := flag.Int("port", 4000, "Set port of the server")
+	mode := flag.String("mode", "rest", "Choose between 'html', 'rest' and 'both'")
+	port1 := flag.Int("port", 3000, "Set primary port of the server")
+	port2 := flag.Int("port2", 4000, "Set secondary port of the server")
 
 	if len(os.Args) < 2 {
 		printUsage()
 		os.Exit(0)
 	}
 	flag.Parse()
-	run(*mode, *port)
+	run(*mode, *port1, *port2)
 }
 
 func printUsage() {
@@ -27,14 +28,18 @@ func printUsage() {
 	flag.PrintDefaults()
 }
 
-func run(mode string, port int) {
+func run(mode string, port1 int, port2 int) {
 	switch mode {
 	case "explorer":
-		fmt.Printf("Run Papicoin Explorer\n\n")
-		explorer.Start(port)
+		fmt.Printf("Run Papicoin Explorer on %d\n\n", port1)
+		explorer.Start(port1)
 	case "rest":
-		fmt.Printf("Run Papicoin REST API server\n\n")
-		rest.Start(port)
+		fmt.Printf("Run Papicoin REST API server on %d\n\n", port1)
+		rest.Start(port1)
+	case "both":
+		fmt.Printf("Run Papicoin Explorer(%d) and REST API(%d)\n\n", port1, port2)
+		go explorer.Start(port1)
+		rest.Start(port2)
 	default:
 		printUsage()
 	}
