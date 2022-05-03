e78bcc25085c2ddf3a1a71d9c7129847685c9501
Minwoo Jeong <jeongmwj@gmail.com>
2022-05-03 20:45:31 +0900

    Update the CLI with `flag` package

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -9,28 +10,33 @@ import (
 )
 
 func main() {
+	mode := flag.String("mode", "rest", "Choose between 'html' and 'rest'")
+	port := flag.Int("port", 4000, "Set port of the server")
+
 	if len(os.Args) < 2 {
 		printUsage()
 		os.Exit(0)
 	}
-	run()
+	flag.Parse()
+	run(*mode, *port)
 }
 
 func printUsage() {
 	fmt.Printf("Welcome to Papicoin!\n\n")
-	fmt.Printf("Please use the following commands.\n\n")
-	fmt.Println("explorer:	Start the HTML Explorer")
-	fmt.Println("rest:		Start the REST API (recommended)")
+	fmt.Printf("Please use the following flags.\n\n")
+	flag.PrintDefaults()
 }
 
-func run() {
-	switch os.Args[1] {
+func run(mode string, port int) {
+
+	fmt.Println(port, mode)
+	switch mode {
 	case "explorer":
 		fmt.Println("Start Explorer")
-		explorer.Start(3000)
+		explorer.Start(port)
 	case "rest":
 		fmt.Println("Start REST API")
-		rest.Start(4000)
+		rest.Start(port)
 	default:
 		printUsage()
 	}
